43ef14e2f4dd90c8da3a4ff7bdad838e62f6ab05
Artur Matysiak <inz.matys@gmail.com>
2026-07-22 17:36:39 +0200

    Second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,11 +6,6 @@ function App() {
 
   return (
     <section id="center">
-      <div className="hero">
-        <img src={heroImg} className="base" width="170" height="179" alt="" />
-        <img src={reactLogo} className="framework" alt="React logo" />
-        <img src={viteLogo} className="vite" alt="Vite logo" />
-      </div>
       <div>
         <h1>Get started Vite + React</h1>
         <p>
